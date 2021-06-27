ab857385418ab4fcf4a84c181244b40b1fc1974e
John Madden <jck13mad@gmail.com>
2021-06-27 08:43:18 -0500

    Added project serializer

---
+++ app/serializer/project_serializer.rb
@@ -0,0 +1,17 @@
+class ProjectSerializer 
+
+    def initialize(project)
+        @project_collection = project
+    end
+
+    def to_serialized_json
+        options = {
+          include: {
+            comments: {}
+          },
+          except: [:created_at, :updated_at]
+        }
+
+        @project_collection.to_json(options)
+      end
+end
